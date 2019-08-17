c15bc4d2f98fe8e7ef37bd34df533bfc9abfba7a
ちはそ <cycyccq@gmail.com>
2019-08-18 00:22:27 +0900

    Add process to count each words (finished)

--- 03_pi.rb
+++ 03_pi.rb
@@ -1,3 +1,8 @@
 sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-separated_sentence = sentence.s(" ")
-p separated_sentence
+
+separated_sentence = sentence.split(" ")
+separated_sentence.each { |s| s.delete!("^[A-z]") }
+
+char_nums = []
+separated_sentence.each { |s| char_nums << s.length }
+puts char_nums.join("")
