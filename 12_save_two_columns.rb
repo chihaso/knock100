59691b87641ecc0ec5f0d346777f7e5975e587bf
chihaso <cycyccq@gmail.com>
2019-08-29 23:38:11 +0900

    add reading/writing columuns func

--- 12_save_two_columns.rb
+++ 12_save_two_columns.rb
@@ -1,3 +1,17 @@
 sentence = File.open(ARGV[0]).read
 lines = sentence.split("\n")
 p lines
+col1 = []
+col2 = []
+lines.each { |line|
+  separated_line = line.split(/[\t ]/)
+  col1 << separated_line[0]
+  col2 << separated_line[1]
+}
+
+File.open("col1.txt", "w") { |f|
+  f.puts col1
+}
+File.open("col2.txt", "w") { |f|
+  f.puts col2
+}
